78442bcf9d14cc72afb4a95af95caee7b62c8d19
Josh Gearheart <other.josh.gearheart@gmail.com>
2019-01-31 12:58:58 -0800

    add historical steps to campaignDateArray

--- src/components/PedometerSensorV2.js
+++ src/components/PedometerSensorV2.js
@@ -1,8 +1,9 @@
 import Expo from "expo";
 import React from "react";
 import { Pedometer } from "expo";
-import { StyleSheet, Text, View, AsyncStorage, AppState } from "react-native";
+import { StyleSheet, Text, View, AsyncStorage, AppState, ScrollView, Button } from "react-native";
 
+import DateShower from './DateShower';
 
 export default class PedometerSensorV2 extends React.Component {
 
@@ -13,16 +14,31 @@ export default class PedometerSensorV2 extends React.Component {
       isPedometerAvailable: "checking",
       todayStepCount: 0,
       yesterdayStepCount: 0,
-      currentStepCount: 0
+      currentStepCount: 0,
+      campaignLength: 15,
+      campaignStartDate: new Date('January 31, 2019 06:00:00'),
+      campaignDateArray: null,
     };
     this._storeData = this._storeData.bind(this);
     this._retrieveData = this._retrieveData.bind(this);
+  }
+
+  componentWillMount() {
+    if (this.state.campaignDateArray === null) {
+      this._constructDateLog();
+    }
   }
 
   componentDidMount() {
     this._subscribe();
     AppState.addEventListener('change', this._handleAppStateChange);
     this._updateStepCounts();
+    setInterval(() => {
+      if (this.state.appState === 'active') {
+        console.log('one more minute down!');
+        this._updateStepCounts();
+      }
+    }, 60000);
   }
 
   componentWillUnmount() {
@@ -38,7 +54,6 @@ export default class PedometerSensorV2 extends React.Component {
 
     Pedometer.isAvailableAsync().then(
       result => {
-
         this.setState({
           isPedometerAvailable: String(result)
         });
@@ -71,6 +86,41 @@ export default class PedometerSensorV2 extends React.Component {
     this._subscription && this._subscription.remove();
     this._subscription = null;
   };
+
+  _constructDateLog = () => {
+    // set variables
+    const { campaignStartDate, campaignLength } = this.state;
+    const dateArray = [];
+    const day1Start = new Date(campaignStartDate);
+    const day1End = new Date(campaignStartDate);
+    day1Start.setHours(6,0,0,0);
+    day1End.setHours(24,0,0,0);
+    // create array of campaign days based on campaignLength and campaignStartDate
+    for (i=0; i < campaignLength; i++) {
+      if (i === 0) {
+        // first iteration has 0 mutations from variables
+        dateArray.push({
+          day: i + 1,
+          start: new Date(day1Start),
+          end: new Date(day1End),
+        });
+      } else {
+        // all iterations after 1 add i days to date variables
+        const start = new Date(day1Start);
+        const end = new Date(day1End);
+        dateArray.push({
+          day: i + 1,
+          start: new Date(start.setDate(start.getDate() + i)),
+          end: new Date(end.setDate(end.getDate() + i)),
+        });
+      }
+    }
+
+    // put that array into state
+    this.setState({
+      campaignDateArray: dateArray
+    })
+  }
 
   _handleAppStateChange = (nextAppState) => {
     if (
@@ -148,6 +198,34 @@ export default class PedometerSensorV2 extends React.Component {
     );
   }
 
+  _updatePastStepCounts = () => {
+    const { campaignDateArray } = this.state;
+    const campaignDateArrayCopy = JSON.parse(JSON.stringify(campaignDateArray))
+
+    campaignDateArrayCopy.forEach((obj, index) => {
+      console.log(obj.start, obj.end);
+      Pedometer.getStepCountAsync(new Date(obj.start), new Date(obj.end)).then(
+        async (result) => {
+          // await this._storeData('day' + (index+1) + 'StepCountStorage', result.steps.toString());
+          // const stepsToAdd = {
+          //   steps: parseInt(await this._retrieveData('yesterdayStepCountStorage'), 10)
+          // }
+          // console.log(stepsToAdd);
+          // const dateWithSteps = Object.assign({}, campaignDateArrayCopy[index], stepsToAdd);
+          // campaignDateArrayCopy.splice(index, 1, dateWithSteps)
+          console.log(index + ': ' + result.steps);
+        },
+        error => {
+          // if we have an error, state shows the error
+          console.log('error at ' + index);
+        }
+      );
+    });
+    this.setState({
+      campaignDateArray: campaignDateArrayCopy
+    });
+  }
+
   async _storeData(keyString, valueString) {
     try {
       await AsyncStorage.setItem(keyString, valueString);
@@ -181,10 +259,23 @@ export default class PedometerSensorV2 extends React.Component {
         <Text>
           Steps taken yesterday: {this.state.yesterdayStepCount}
         </Text>
+        <Button title='update past steps'
+          onPress={this._updatePastStepCounts} />
+        <ScrollView>
+          {this.state.campaignDateArray.map((dateObj, index) =>
+            <DateShower day={dateObj.day}
+              start={dateObj.start.toString()}
+              end={dateObj.end.toString()}
+              steps={dateObj.steps}
+              key={index} />
+          )}
+        </ScrollView>
       </View>
     );
   }
 }
+
+//THIS IS FROM THE EXPO EXAMPLE
 // <Text>Walk! And watch this go up: {this.state.currentStepCount}</Text>
 
 const styles = StyleSheet.create({
@@ -193,5 +284,5 @@ const styles = StyleSheet.create({
     marginTop: 15,
     alignItems: "center",
     justifyContent: "center"
-  }
+  },
 });
