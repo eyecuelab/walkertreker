34c87bccb0f5b205ecf490091b8c11579deaa6ba
Josh Gearheart <other.josh.gearheart@gmail.com>
2019-02-04 13:13:19 -0800

    add action creator index

---
+++ src/components/actions/index.js
@@ -0,0 +1,26 @@
+import constants from './constants';
+const { c } = constants;
+
+export const setAppState = (appState) => ({
+  type: c.NEW_APP_STATE,
+  appState,
+});
+
+export const setCampaignDates = (firstDayStart, firstDayEnd, campaignLength) => {
+
+  const dateArray = [];
+
+  for (let i=0; i < campaignLength; i++) {
+      const start = new Date(firstDayStart); /* using campaignStartDate directly here caused the app to crash... not sure why*/
+      const end = new Date(firstDayEnd); /* using campaignStartDate directly here caused the app to crash... not sure why*/
+      dateArray.push({
+        day: i + 1,
+        start: new Date(start.setDate(start.getDate() + i)),
+        end: new Date(end.setDate(end.getDate() + i)),
+      });
+  }
+  return ({
+    type: c.SET_CAMPAIGN_DATES,
+    campaignDateArray: dateArray,
+  });
+}
