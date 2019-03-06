91dc0de10656f053e0d1aa8205939a6aa68bdbeb
Josh Gearheart <other.josh.gearheart@gmail.com>
2019-03-06 11:47:51 -0800

    connect safehouse to store

--- src/components/screens/Safehouse.js
+++ src/components/screens/Safehouse.js
@@ -2,11 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { connect } from 'react-redux';
+
+import constants from '../../constants';
+const { c } = constants;
 
 import defaultStyle from '../../styles/defaultStyle';
 import SingleButtonFullWidth from '../ui/SingleButtonFullWidth';
 
-export default class Safehouse extends React.Component {
+class Safehouse extends React.Component {
 
   _selectFood = () => {
     const { dispatch } = this.props;
@@ -119,3 +123,14 @@ const customStyles = StyleSheet.create({
     width: '100%'
   }
 })
+
+function mapStateToProps(state) {
+  return {
+    // appState: state.appState,
+    steps: state.steps,
+    // campaign: state.campaign,
+    // player: state.player,
+  }
+}
+
+export default connect(mapStateToProps)(Safehouse);
