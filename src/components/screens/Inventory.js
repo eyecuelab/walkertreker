dd9cf8a684df56d30699b1d8418fc70a75b6a921
Josh Gearheart <other.josh.gearheart@gmail.com>
2019-03-08 12:24:12 -0800

    add initial modal to inventory

--- src/components/screens/Inventory.js
+++ src/components/screens/Inventory.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, ImageBackground, ScrollView, Image } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { connect } from 'react-redux';
+import Modal from 'react-native-modal';
 import constants from '../../constants';
 const { c, item } = constants;
 const { foodArray, medicineArray, weaponArray } = item;
@@ -10,8 +11,25 @@ import defaultStyle from '../../styles/defaultStyle';
 import DayCounter from '../ui/DayCounter';
 import TwoButtonOverlay from '../ui/TwoButtonOverlay';
 import SingleButtonFullWidth from '../ui/SingleButtonFullWidth';
+import DistressedImageHolder from '../ui/DistressedImageHolder';
+
+const bg1 = require('../../../assets/buttontexture1.png');
+const bg2 = require('../../../assets/buttontexture2.png');
+const bg3 = require('../../../assets/buttontexture3.png');
 
 class Inventory extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      foodModalVisible: false,
+      medicineModalVisible: false,
+      weaponModalVisible: false,
+      foodModalIndex: null,
+      medicineModalIndex: null,
+      weaponModalIndex: null,
+    }
+  }
 
   _onButtonPressSummary = () => {
     this.props.navigation.navigate('CampaignSummary');
@@ -19,6 +37,21 @@ class Inventory extends React.Component {
 
   _onButtonPressSafehouse = () => {
     this.props.navigation.navigate('Safehouse');
+  }
+
+  _toggleFoodModal = () => {
+    const foodModalVisible = !this.state.foodModalVisible;
+    this.setState({ foodModalVisible })
+  }
+
+  _toggleMedicineModal = () => {
+    const medicineModalVisible = !this.state.medicineModalVisible;
+    this.setState({ medicineModalVisible })
+  }
+
+  _toggleWeaponModal = () => {
+    const weaponModalVisible = !this.state.weaponModalVisible;
+    this.setState({ weaponModalVisible })
   }
 
   _submitConditionalRender = () => {
@@ -47,11 +80,23 @@ class Inventory extends React.Component {
     }
   }
 
+  // TODO:
+  // [ ] create modals for all three item types
+  // [ ] when an item is pressed, first assign its index to state and then give the modal access to that state
+  // [ ] write functions in food and med modals that use the index passed in to remove that item from the inventory array, adjust health/hunger, update the player, and update the campaign
+
   render() {
     return (
       <ImageBackground
         source={this.props.screenProps.backgroundImage}
         style={{width: '100%', height: '100%'}}>
+
+        <Modal isVisible={this.state.foodtModalVisible}>
+          <FoodModal
+            handleModalStateChange={this._toggleFoodModal}
+            index={} />
+        </Modal>
+
         <View style={[styles.container, {alignItems: 'flex-start'}]}>
           <DayCounter campaign={this.props.campaign} />
           <Text style={styles.headline}>GROUP INVENTORY</Text>
@@ -61,44 +106,52 @@ class Inventory extends React.Component {
 
               <Text style={styles.subHeading}>Food</Text>
               <View style={customStyles.itemContainer}>
+
                 {this.props.campaign.inventory.foodItems.map((number, index) => {
                   const img = foodArray[number];
                   return(
-                    <Image
-                      key={index}
-                      style={{width: widthUnit*28, height: widthUnit*28, marginRight: widthUnit}}
-                      resizeMode='contain'
-                      source={img} />
+                    {/*TODO: add */}
+                      <Image
+                        key={index}
+                        style={customStyles.item}
+                        resizeMode='contain'
+                        source={img} />
                   )
                 })}
+
               </View>
 
               <Text style={styles.subHeading}>Medicine</Text>
               <View style={customStyles.itemContainer}>
+
                 {this.props.campaign.inventory.medicineItems.map((number, index) => {
                   const img = medicineArray[number];
                   return(
                     <Image
                       key={index}
-                      style={{width: widthUnit*28, height: widthUnit*28, marginRight: widthUnit}}
+                      index={index}
+                      style={customStyles.item}
                       resizeMode='contain'
                       source={img} />
                   )
                 })}
+
               </View>
 
               <Text style={styles.subHeading}>Weapons</Text>
               <View style={customStyles.itemContainer}>
+
                 {this.props.campaign.inventory.weaponItems.map((number, index) => {
                   const img = weaponArray[number];
                   return(
                     <Image
                       key={index}
-                      style={{width: widthUnit*28, height: widthUnit*28, marginRight: widthUnit}}
+                      style={customStyles.item}
                       resizeMode='contain'
                       source={img} />
                   )
                 })}
+
               </View>
 
             </ScrollView>
@@ -133,7 +186,6 @@ const customStyles = StyleSheet.create({
     justifyContent: 'flex-end',
     alignItems: 'center',
     marginBottom: widthUnit*2,
-    backgroundColor: 'pink',
   },
   inventoryContainer: {
     flex: 1,
@@ -141,7 +193,6 @@ const customStyles = StyleSheet.create({
     // flexDirection: 'row',
     // alignItems: 'flex-start',
     flexWrap: 'wrap',
-    backgroundColor: 'lime',
   },
   itemContainer: {
     flex: 1,
@@ -155,12 +206,17 @@ const customStyles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  item: {
+    width: widthUnit*28,
+    height: widthUnit*28,
+    marginRight: widthUnit
+  }
 });
 
 function mapStateToProps(state) {
   return {
     campaign: state.campaign,
-    // player: state.player,
+    player: state.player,
     steps: state.steps,
   }
 }
