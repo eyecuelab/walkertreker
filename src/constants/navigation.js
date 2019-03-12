255c6ee6fd1d7d2651224127dfc667dcfccc9d46
Joe Friesen <friesen.josephc@gmail.com>
2019-03-12 15:59:30 -0700

    Add WaitForStart screen. Clean up initial navigation (yet again...)

--- src/constants/navigation.js
+++ src/constants/navigation.js
@@ -4,13 +4,10 @@ export const NAVIGATION = {
   campaignIsLost: 'EndOfGame',
   campaignIsWon: 'EndOfGame',
   randomEventStart: 'RandomEvent',
-  randomEventEnd: 'CampaignSummary',
+  randomEventEnd: 'RandomEventResult',
   endOfDayUpdate: 'EndOfDaySummary',
   noPlayerSteps: 'none',
   hungerAlert: 'none',
 }
 
 // NAVIGATION[type] returns one of the above, a route we would like the app to navigate to. If NAVIGATION[type] == 'none', we do not want to trigger a navigation event. Keeping this in a constants file and sharing it with both Start.js initial navigation and NotificationListeners that fire when the app is already open
-
-// noPlayerSteps: no navigation, just ask player to open the app to update steps
-// hungerAlert: no navigation, alert player that they are under 20 hunger and need to eat
