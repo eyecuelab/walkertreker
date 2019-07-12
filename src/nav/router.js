4ac4786a45d2535fc60a7e7b42a90939cca438bb
wc <wchamberlain89@gmail.com>
2019-07-12 10:50:23 -0700

    fixed merge bugs

--- src/nav/router.js
+++ src/nav/router.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { createStackNavigator, createAppContainer, createSwitchNavigator, createBottomTabNavigator } from "react-navigation";
-import { connect } from 'react-redux';
-import { Linking } from 'expo';
 import constants from './../constants';
-const { c } = constants;
 import {Image} from 'react-native';
 
 // screens
@@ -26,10 +23,8 @@ import AuthCheck from './../components/screens/AuthCheck';
 import SignUp from './../components/screens/SignUp';
 import AccountRecovery from './../components/ui/AccountRecovery';
 import MainAppRouter from './../components/screens/MainAppRouter';
-import Journal from './../components/screens/Journal';
 import Lobby from './../components/screens/Lobby';
-import {store} from './../store';
-import TabBarIcon from '../components/ui/TabBarIcon';
+
 
 const AuthStack = createStackNavigator(
   {
@@ -114,31 +109,18 @@ const MainApp = createSwitchNavigator(
   {
     MainAppRouter: { screen: MainAppRouter },
     CreateCampaign: { screen : CreateCampaign },
-<<<<<<< HEAD
     Lobby : { screen : LobbyNavigator},
     Campaign : { screen : CampaignNavigator },
     Join: {
       screen: AcceptInvite,
       path: 'join'
     }, 
-=======
-    CampaignStaging: { screen : CampaignStaging },
-    CampaignSummary: { screen : CampaignSummary },
-    InvitePlayers: { screen : InvitePlayers },
-    RandomEvent: { screen: RandomEvent },
-    RandomEventResult: { screen: RandomEventResult },
-    Inventory: { screen: Inventory }, 
-    Journal: { screen: Journal },
->>>>>>> master
   },
   {
     defaultNavigationOptions: {
       header: null,
     },
-<<<<<<< HEAD
     initialRouteName: "MainAppRouter" 
-=======
->>>>>>> master
   }
 )
 
