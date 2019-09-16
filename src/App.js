a6b11a9407121861f1aba96db3f2e49b8b21295a
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-16 12:37:05 -0700

    add boilerplate BackgroundFetch code from react-natove-background-fetch docs

--- src/App.js
+++ src/App.js
@@ -161,6 +161,48 @@ class App extends React.Component {
         queryParams
       });
     }
+    BackgroundFetch.configure(
+      {
+        minimumFetchInterval: 15, // <-- minutes (15 is minimum allowed)
+        // Android options
+        stopOnTerminate: false,
+        startOnBoot: true,
+        requiredNetworkType: BackgroundFetch.NETWORK_TYPE_NONE, // Default
+        requiresCharging: false, // Default
+        requiresDeviceIdle: false, // Default
+        requiresBatteryNotLow: true, // Default
+        requiresStorageNotLow: false // Default
+      },
+      () => {
+        console.log("[js] Received background-fetch event");
+        // Required: Signal completion of your task to native code
+        // If you fail to do this, the OS can terminate your app
+        // or assign battery-blame for consuming too much background-time
+        BackgroundFetch.finish(BackgroundFetch.FETCH_RESULT_NEW_DATA);
+      },
+      error => {
+        console.log("[js] RNBackgroundFetch failed to start", error);
+      }
+    );
+
+    BackgroundFetch.status(status => {
+      switch (status) {
+        case BackgroundFetch.STATUS_RESTRICTED:
+          console.log("BackgroundFetch restricted");
+          break;
+        case BackgroundFetch.STATUS_DENIED:
+          console.log("BackgroundFetch denied");
+          break;
+        case BackgroundFetch.STATUS_AVAILABLE:
+          console.log("BackgroundFetch is enabled");
+          break;
+        default:
+          console.log(
+            "default case for bg fetch: not restricted, denied or enable."
+          );
+          break;
+      }
+    });
   }
 
   componentDidUpdate() {
