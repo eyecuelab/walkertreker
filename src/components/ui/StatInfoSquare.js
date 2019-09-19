6d3c1d80196fbc8e3c2c6238ca829c7b4f83c8e5
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-19 13:53:18 -0700

    change statInfoSquare to a functional component, update imports

--- src/components/ui/StatInfoSquare.js
+++ src/components/ui/StatInfoSquare.js
@@ -1,41 +1,45 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Label } from './../text';
-const bg1 = require('../../../assets/buttontexture1.png');
-import { View, Text, StyleSheet, ImageBackground } from 'react-native';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-const heightUnit = hp('1%');
-const widthUnit = wp('1%');
+import React from "react";
+import {
+  View,
+  /* Text, */ StyleSheet /* , ImageBackground */
+} from "react-native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen";
+import PropTypes from "prop-types";
+import { Label } from "../text";
 
-class StatInfoSquare extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-          <Label style={[styles.text, {color: "#FF0000"}]}>{this.props.label}</Label>
-          <Label style={styles.text}>{this.props.value}</Label>
-      </View>
+// const bg1 = require('../../../assets/buttontexture1.png');
 
-    );
-  }
-}
+// const heightUnit = hp('1%');
+
+const widthUnit = wp("1%");
 
 const styles = StyleSheet.create({
   container: {
-    minWidth: widthUnit*30,
-    justifyContent: 'center',
-    alignContent: 'center',
-    paddingHorizontal: widthUnit*7,
-    paddingVertical: widthUnit*5,
+    minWidth: widthUnit * 30,
+    justifyContent: "center",
+    alignContent: "center",
+    paddingHorizontal: widthUnit * 7,
+    paddingVertical: widthUnit * 5,
     backgroundColor: "#000"
   },
   text: {
-    textAlign: 'center'
+    textAlign: "center"
   }
 });
 
-StatInfoSquare.propTypes = {
-  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired, 
-  label: PropTypes.string.isRequired
+export default function StatInfoSquare(props) {
+  return (
+    <View style={styles.container}>
+      <Label style={[styles.text, { color: "#FF0000" }]}>{props.label}</Label>
+      <Label style={styles.text}>{props.value}</Label>
+    </View>
+  );
 }
 
-export default StatInfoSquare;
+StatInfoSquare.propTypes = {
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  label: PropTypes.string.isRequired
+};
