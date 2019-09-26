bbcc0130c7af54c2c73ebf5b0db61b5486300ea0
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-26 16:30:26 -0700

    add proptypes, update code to meet linter rules

--- src/components/ui/JournalUI/JournalEntry.js
+++ src/components/ui/JournalUI/JournalEntry.js
@@ -1,93 +1,117 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, ScrollView } from 'react-native';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import constants from '../../../constants';
+import React from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, View, ImageBackground } from "react-native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen";
+import constants from "../../../constants";
+import { MainText, TextWithBackground } from "../../text";
+
 const { paint } = constants;
 const { paintArray } = paint;
-const paper = require('../../../../assets/burnt-paper.png');
+const paper = require("../../../../assets/burnt-paper.png");
 
-
-import { MainText, TextWithBackground } from '../../text';
-
-class JournalEntry extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  componentWillMount() {
-    this.props.entry ? this.entries = this.props.entry.split('//') : null;
-  }
-
-  getBGimage = () => {
-    const num = Math.floor(Math.random()*paintArray.length)
-    return paintArray[num]
-  }
-
-  render() {
-    return (
-      this.props.entry ? 
-
-      <ImageBackground 
-        source={paper}
-        resizeMode={'stretch'}
-        style={customStyles.paperBg} >
-
-      { this.entries[1] ? 
-        <View style={customStyles.entryBox} >
-          <View>
-            <MainText style={customStyles.entryLine}>{this.entries[0]}</MainText>
-            {this.props.eventNumber && this.props.votingList.length ? 
-                <View style={customStyles.playerVotes}>
-                  {this.props.votingList.map((vote, index) => {
-                    return <TextWithBackground key={index} text={vote} image={this.getBGimage()}/>
-                  })}
-                </View>
-              : null }
-            <MainText style={customStyles.entryLine}>{this.entries[1]}</MainText>
-          </View>
-        </View>
-
-        : <View style={customStyles.dailyEntryBox} >
-            <MainText style={customStyles.entryLine}>{this.entries[0]}</MainText>
-          </View>
-      }
-
-      </ImageBackground> : null
-    )
-  }
-}
-
-const widthUnit = wp('1%');
-const heightUnit = hp('1%');
+const widthUnit = wp("1%");
+const heightUnit = hp("1%");
 const customStyles = StyleSheet.create({
   playerVotes: {
-    paddingHorizontal: widthUnit*2,
+    paddingHorizontal: widthUnit * 2
   },
   entryBox: {
-    paddingHorizontal: heightUnit*4,
-    paddingVertical: heightUnit*6,
+    paddingHorizontal: heightUnit * 4,
+    paddingVertical: heightUnit * 6,
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center"
   },
   dailyEntryBox: {
-    paddingHorizontal: heightUnit*4,
-    paddingVertical: heightUnit*3,
+    paddingHorizontal: heightUnit * 4,
+    paddingVertical: heightUnit * 3,
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center"
   },
   entryLine: {
-    fontSize: widthUnit*6,
-    color: 'black',
+    fontSize: widthUnit * 6,
+    color: "black"
   },
   paperBg: {
     width: undefined,
     height: undefined,
-    marginBottom: heightUnit*2,
+    marginBottom: heightUnit * 2
+  }
+});
+
+class JournalEntry extends React.Component {
+  componentWillMount() {
+    if (this.props.entry) {
+      this.entries = this.props.entry.split("//");
+    }
   }
 
-})
+  getBGimage = () => {
+    const num = Math.floor(Math.random() * paintArray.length);
+    return paintArray[num];
+  };
+
+  render() {
+    return this.props.entry ? (
+      <ImageBackground
+        source={paper}
+        resizeMode="stretch"
+        style={customStyles.paperBg}
+      >
+        {this.entries[1] ? (
+          <View style={customStyles.entryBox}>
+            <View>
+              <MainText style={customStyles.entryLine}>
+                {this.entries[0]}
+              </MainText>
+              {this.props.eventNumber && this.props.votingList.length ? (
+                <View style={customStyles.playerVotes}>
+                  {this.props.votingList.map((vote, index) => {
+                    /* eslint-disable */
+                    return (
+                      <TextWithBackground
+                        key={index}
+                        text={vote}
+                        image={this.getBGimage()}
+                      />
+                    );
+                    /* eslint-enable */
+                  })}
+                </View>
+              ) : null}
+              <MainText style={customStyles.entryLine}>
+                {this.entries[1]}
+              </MainText>
+            </View>
+          </View>
+        ) : (
+          <View style={customStyles.dailyEntryBox}>
+            <MainText style={customStyles.entryLine}>
+              {this.entries[0]}
+            </MainText>
+          </View>
+        )}
+      </ImageBackground>
+    ) : null;
+  }
+}
 
 export default JournalEntry;
+
+/* eslint-disable */
+JournalEntry.propTypes = {
+  entry: PropTypes.string,
+  votingList: PropTypes.arrayOf(PropTypes.any),
+  eventNumber: PropTypes.number
+};
+/* eslint-enable */
+
+JournalEntry.defaultTypes = {
+  entry: "",
+  votingList: [],
+  eventNumber: 0
+};
