d5e7a2daffefdd93011607244182b0527017fbb3
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-27 11:45:02 -0700

    update deaultProps/propTypes, update code to meet linter

--- src/components/ui/ButtonToggle.js
+++ src/components/ui/ButtonToggle.js
@@ -1,36 +1,86 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { StyleSheet, Text, View, ImageBackground, } from 'react-native';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import React from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import {
+  widthPercentageToDP as wp
+  /* heightPercentageToDP as hp */
+} from "react-native-responsive-screen";
+
+const widthUnit = wp("1%");
+// const heightUnit = hp('1%');
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 6
+  },
+  active: {
+    color: "darkred"
+  },
+  inactive: {
+    color: "white"
+  },
+  valueContainer: {
+    flex: 9,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  labelContainer: {
+    flex: 5,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  value: {
+    fontFamily: "gore",
+    fontSize: widthUnit * 5
+  },
+  bigValue: {
+    fontFamily: "gore",
+    fontSize: widthUnit * 7
+  },
+  label: {
+    fontFamily: "gore",
+    fontSize: widthUnit * 4
+  },
+  red: {
+    color: "darkred"
+  },
+  green: {
+    color: "#68ae5b"
+  }
+});
 
 export default class ButtonToggle extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     const toggle = this.props.active ? styles.active : styles.inactive;
     const valueFontStyle = this.props.bigValue ? styles.bigValue : styles.value;
-    const activeTint = this.props.active ? 'white' : 'black';
-    const color = (color) => {
-      if (color == 'red') {
+    const activeTint = this.props.active ? "white" : "black";
+    const color = colorProps => {
+      if (colorProps === "red") {
         return styles.red;
-      } else if (color == 'green') {
-        return styles.green;
-      } else {
-        return;
       }
+      if (colorProps === "green") {
+        return styles.green;
+      }
+      return styles.red;
     };
 
     return (
-      <ImageBackground style={{width: '100%', height: '100%'}} source={this.props.background} tintColor={activeTint}>
-        <View style={[styles.container, toggle]} >
-          
-            <Text style={[toggle, valueFontStyle, color(this.props.color)]}>{this.props.value}</Text>
-          
-          
-            <Text style={[styles.label, toggle, color(this.props.color)]}>{this.props.label}</Text>
-          
+      <ImageBackground
+        style={{ width: "100%", height: "100%" }}
+        source={this.props.background}
+        tintColor={activeTint}
+      >
+        <View style={[styles.container, toggle]}>
+          <Text style={[toggle, valueFontStyle, color(this.props.color)]}>
+            {this.props.value}
+          </Text>
+
+          <Text style={[styles.label, toggle, color(this.props.color)]}>
+            {this.props.label}
+          </Text>
         </View>
       </ImageBackground>
     );
@@ -42,58 +92,20 @@ ButtonToggle.propTypes = {
   bigValue: PropTypes.bool,
   label: PropTypes.string,
   active: PropTypes.bool,
-  background: PropTypes.any,
+  color: PropTypes.string,
+  /* eslint-disable */
+  background: PropTypes.any
+  /* eslint-enable */
 };
 
-//This only toggles when rendered from the CreateCampaign component but not from the CampaignSummary component
+// This only toggles when rendered from the CreateCampaign component
+// but not from the CampaignSummary component
 
 ButtonToggle.defaultProps = {
   active: false,
   bigValue: false,
+  value: "",
+  label: "",
+  color: "",
+  background: ""
 };
-
-const widthUnit = wp('1%');
-const heightUnit = hp('1%');
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 6,
-  },
-  active: {
-    color: 'darkred',
-  },
-  inactive: {
-    color: 'white',
-  },
-  valueContainer: {
-    flex: 9,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  labelContainer: {
-    flex: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  value: {
-    fontFamily: 'gore',
-    fontSize: widthUnit*5,
-  },
-  bigValue: {
-    fontFamily: 'gore',
-    fontSize: widthUnit*7,
-  },
-  label: {
-    fontFamily: 'gore',
-    fontSize: widthUnit*4,
-  },
-  red: {
-    color: 'darkred'
-  },
-  green: {
-    color: '#68ae5b'
-  },
-});
