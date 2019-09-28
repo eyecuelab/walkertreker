5ed61026a70dde4cf42c7f8aada730a383a28850
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-27 18:47:07 -0700

    convert from class to functional component, update code to meet linter

--- src/components/ui/SingleButtonFullWidth.js
+++ src/components/ui/SingleButtonFullWidth.js
@@ -1,38 +1,51 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { StyleSheet, Text, View, TouchableHighlight, } from 'react-native';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import React from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, Text, TouchableHighlight } from "react-native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen";
+import defaultStyle from "../../styles/defaultStyle";
 
-import defaultStyle from '../../styles/defaultStyle';
-
-export default class SingleButtonFullWidth extends React.Component {
-
-  render() {
-    return(
-      <TouchableHighlight
-        style={[customStyles.container, {backgroundColor: this.props.backgroundColor}, {marginTop: this.props.marginTop || 0}]}
-        onPress={this.props.onButtonPress}
-      >
-        <Text style={styles.label}>{this.props.title}</Text>
-      </TouchableHighlight>
-    )
-  }
-}
+const bg1 = require("../../../assets/buttontexture1.png");
 
 const styles = StyleSheet.create(defaultStyle);
-const widthUnit = wp('1%');
-const heightUnit = hp('1%');
+const widthUnit = wp("1%");
+const heightUnit = hp("1%");
 const customStyles = StyleSheet.create({
   container: {
-    width: '100%',
-    height: heightUnit*8,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+    width: "100%",
+    height: heightUnit * 8,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+export default function SingleButtonFullWidth(props) {
+  return (
+    <TouchableHighlight
+      style={[
+        customStyles.container,
+        { backgroundColor: props.backgroundColor },
+        { marginTop: props.marginTop || 0 }
+      ]}
+      onPress={props.onButtonPress}
+    >
+      <Text style={styles.label}>{props.title}</Text>
+    </TouchableHighlight>
+  );
+}
 
 SingleButtonFullWidth.propTypes = {
   title: PropTypes.string,
   backgroundColor: PropTypes.string,
   onButtonPress: PropTypes.func,
-}
+  marginTop: PropTypes.number
+};
+
+SingleButtonFullWidth.defaultProps = {
+  title: "",
+  backgroundColor: "black",
+  onButtonPress: () => {},
+  marginTop: 0
+};
